13bad72a158c7548551f00a89e3057e5ef1457aa
cec91 <vincenzo.santucci1@gmail.com>
2017-05-16 16:39:39 +0200

    with hibernate 4

--- src/main/java/Utils/HibernateAdapter.java
+++ src/main/java/Utils/HibernateAdapter.java
@@ -5,6 +5,8 @@ import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.service.ServiceRegistry;
 
+import java.io.File;
+
 /**
  * Created by vsantucc on 16/05/2017.
  */
@@ -17,8 +19,11 @@ public class HibernateAdapter {
 
     public SessionFactory getAdapter() {
 
+        ClassLoader classLoader = getClass().getClassLoader();
+     
+        Configuration configuration = new Configuration().configure(classLoader.getResource("hibernate.cfg.xml"));
         SessionFactory sessionFactory;
-        Configuration configuration = new Configuration().configure(HibernateAdapter.class.getResource("/hibernate.cfg.xml"));
+       // Configuration configuration = new Configuration().configure(HibernateAdapter.class.getResource("/hibernate.cfg.xml"));
         StandardServiceRegistryBuilder serviceRegistryBuilder = new StandardServiceRegistryBuilder();
         serviceRegistryBuilder.applySettings(configuration.getProperties());
         ServiceRegistry serviceRegistry = serviceRegistryBuilder.build();
