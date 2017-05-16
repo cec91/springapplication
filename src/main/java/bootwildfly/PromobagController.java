5055eb3162a0acb4dfaa525c75d3835105361c6c
cec91 <vincenzo.santucci1@gmail.com>
2017-05-16 14:19:01 +0200

    health check status

--- src/main/java/bootwildfly/PromobagController.java
+++ src/main/java/bootwildfly/PromobagController.java
@@ -4,11 +4,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloWildFlyController {
+public class PromobagController {
 
 
     @RequestMapping("hello")
     public String sayHello(){
         return ("Hello, SpringBoot on Wildfly");
+    }
+
+    @RequestMapping("/health/check")
+    public String healthCheck(){
+        return "Status OK - Promobag server";
     }
 }
